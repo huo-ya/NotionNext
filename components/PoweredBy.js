c88492717dcc4b050b6a4777d5c04060ef755a6d
yaosen <210828149+huo-ya@users.noreply.github.com>
2026-03-20 19:37:21 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -7,7 +7,6 @@ import { siteConfig } from '@/lib/config'
 export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm ${props.className || ''}`}>
-      © 2026-2099 干货大师
     </div>
   )
 }
