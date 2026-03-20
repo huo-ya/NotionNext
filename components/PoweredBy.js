30b72ba1b9ba65e73cb3eeae1957d7a69a006dca
yaosen <210828149+huo-ya@users.noreply.github.com>
2026-03-20 19:41:36 +0800

    Remove PoweredBy component
    Removed the PoweredBy component and its associated imports.

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -1,12 +1,1 @@
-import { siteConfig } from '@/lib/config'
 
-/**
- * 驱动版权
- * @returns
- */
-export default function PoweredBy(props) {
-  return (
-    <div className={`inline text-sm ${props.className || ''}`}>
-    </div>
-  )
-}
